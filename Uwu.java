12eb61fb5b70b8af976ecc5e7e0613182d74d700
scintilla4evr <kacper17@outlook.com>
2020-04-10 15:36:43 +0200

    podział ról

--- Uwu.java
+++ Uwu.java
@@ -1,3 +1,21 @@
+/*
+#to_do:
+- rysowanie planszy
+- lol grafika testowa (obrazki lol pobraliście affinity?)
+- łażenie po planszy
+- lol kolizja
+- drzwi
+- pliki poziomów
+
+Ola: drzwi 
+Martyna - Let me speak to your manager... : łażenie po planszy //key listener i zmienia w graczu x i y i potem repaint()
+Kacper S: plik poziomów i edytor poziomów
+Kacper A: lol KOLIZJA
+Hubert: Grafika testowa lol - gracz, podloge, sciana
+Artur: rysowanie planszy
+
+*/
+
 
 public interface IItem {
     private String name;
