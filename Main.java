01a7702fc4ecdf4b8935c5880588fd2988a70c18
scintilla4evr <kacper17@outlook.com>
2020-04-23 13:38:05 +0200

    Przybita pieczęć sci approved
    Co-authored-by: mjakubowska <martynajakubowska05@gmail.com>
    Co-authored-by: lilartisto <orginalartis@gmail.com>
    Co-authored-by: Hubert Nakielski <hubert.nakielski@gmail.com>

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Main {
- public static void main(String[] args ){
+    public static void main(String[] args ){
 
         Game game = new Game();
         Player player = new Player(game);
